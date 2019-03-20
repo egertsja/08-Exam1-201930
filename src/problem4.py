11cf5fa25fcb6b55272a08c7f3ff160893f99271
egertsja <jae242000@gmail.com>
2019-03-19 21:35:16 -0400

    DONE

--- src/problem4.py
+++ src/problem4.py
@@ -3,8 +3,8 @@ Exam 1, problem 4.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Jonah Egertson.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -80,10 +80,30 @@ def problem4(point1, point2, n, window):
       :type window:  rg.RoseWindow
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # IMPORTANT: For PARTIAL CREDIT, ignore the colors.
     # -------------------------------------------------------------------------
+    point1.attach_to(window)
+    point2.attach_to(window)
+    dx = (point2.x-point1.x)/(2*n)
+    dy = (point2.y-point1.y)/(2*n)
+    px = point1.x
+    py = point1.y
+    for _ in range(n):
+        pnt = rg.Point(px+dx,py+dy)
+        pnt.fill_color = point2.fill_color
+        px = pnt.x
+        py = pnt.y
+        pnt.attach_to(window)
+    for _ in range(n):
+        pnt = rg.Point(px + dx, py + dy)
+        pnt.fill_color = point1.fill_color
+        px = pnt.x
+        py = pnt.y
+        pnt.attach_to(window)
+    window.render()
+
 
 
 # -----------------------------------------------------------------------------
